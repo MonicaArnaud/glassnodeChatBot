52afdef6bd486fbd5d6be860cb0345c2a7614f47
MonicaArnaud <58461368+MonicaArnaud@users.noreply.github.com>
2023-05-29 00:05:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,69 +25,17 @@ def chat_with_model():
     
     result = qa({"question": question, "chat_history": chat_history})
     chat_history.append((question, result["answer"]))
-    st.write(result)
     
-# def chat_with_model(messages):
-#     response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=messages,
-#         max_tokens=500,
-#     )
-#     return response.choices[0].message.content
-
-#def find_related_documents(query):
-#     docs_with_scores_and_content_texts = vectordb.similarity_search_with_score(query)[:3]
-#     return [doc.page_content for doc,_ in docs_with_scores_and_content_texts]
-
+    st.subheader("Chatbot Response:")
+    st.write(result["answer"])
+    
 
 # Streamlit app code
 def main():
     st.title("Glassnode Chatbot Demo")
-
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = []
-
-        initial_message ={
-            "role": "system",
-            "content": "You are a smart information retrieval AI. Below you will see questions from user, and several related items for the question. Based on the documents, answer the question. If you cannot answer the question based on the documents, just say you don't know. Don't try to make up answers."
-            }
-
-        st.session_state['messages'].append(initial_message)
-
-    chat_history_container = st.container()
-
-    with chat_history_container:
-        for message in st.session_state["messages"]:
-            if(message["role"]=="user"):
-                st.markdown(f"**You**: {message['content']}")
-            elif (message["role"]=="assistant"):
-                st.markdown(f"_Assistant_: {message['content']}")
-
-        st.write("\nType your message below:")
-        user_input = st.text_input("Enter", key=len(st.session_state["messages"]))
-
-    if user_input:
-        # Reset the chat history
-        st.session_state['messages'] = []
-        # Append user message to the conversation
-        new_message = {"role": "user", "content": user_input}
-        st.session_state['messages'].append(new_message)
-
-        # Find related documents based on user input and add them as messages
-        related_documents = find_related_documents(user_input)
-
-        if len(related_documents) > 0:
-            doc_messages = [{"role": "system", "content": doc_text} for doc_text in related_documents]
-            st.session_state["messages"].extend(doc_messages)
-
-        # Get chatbot's response and append it to the conversation
-        bot_response = chat_with_model(st.session_state['messages'])
-
-        assistant_response = {"role": "assistant", "content": bot_response}
-        st.session_state["messages"].append(assistant_response)
-
-        # We need to manually rerun Streamlit script so that Streamlit can process new state & display it
-        st.experimental_rerun()
+    st.write("Type your question and press Enter:")
+    chat_with_model()
+    # st.experimental_rerun()
 
 if __name__ == "__main__":
     main()
