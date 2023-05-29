66fc58e63dec395615806e6631fbfc948a7dd50b
MonicaArnaud <58461368+MonicaArnaud@users.noreply.github.com>
2023-05-29 12:18:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ vectordb_retriver = vectordb.as_retriever(search_kwargs={"k":3})
 bot_msg_container_html_template = '''
 <div style='background-color: #FFFFFF; padding: 10px; border-radius: 5px; margin-bottom: 10px; display: flex'>
     <div style="width: 20%; display: flex; justify-content: center">
-        <img src="https://imgur.com/wTTSggA" style="max-height: 50px; max-width: 50px; border-radius: 50%;">
+        <img src="glassnodeChatPhoto/glassnode.png" style="max-height: 50px; max-width: 50px; border-radius: 50%;">
     </div>
     <div style="width: 80%;">
         $MSG
@@ -80,7 +80,7 @@ user_msg_container_html_template = '''
         $MSG
     </div>
     <div style="width: 20%; margin-left: auto; display: flex; justify-content: center;">
-        <img src="https://imgur.com/mnaOKkl" style="max-width: 50px; max-height: 50px; float: right; border-radius: 50%;">
+        <img src="glassnodeChatPhoto/userProfil.jpg" style="max-width: 50px; max-height: 50px; float: right; border-radius: 50%;">
     </div>    
 </div>
 '''
